a53436cbad340b85fc304641a019a58640f8c1e2
Pearl <w2crosss@gmail.com>
2025-09-28 05:57:20 +0900

    FIX: vue파일 인식되도록 설정

---
+++ src/shims-vue.d.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+ 
+
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<object, {}, unknown>;
+  export default component;
+}
