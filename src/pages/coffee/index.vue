c6fff470f88bd873eeb2b9681a58a5eb981d6e55
Pearl <w2crosss@gmail.com>
2025-10-24 05:27:53 +0900

    Add: COFFEE 메뉴 관련 화면 생성

---
+++ src/pages/coffee/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+import SimpleHeaderWrap from "@/components/layout/header/SimpleHeaderWrap.vue";
+
+const folderName = import.meta.url.split("/").at(-2).toUpperCase();
+</script>
+
+<template>
+  <div class="index">
+    <SimpleHeaderWrap :title="folderName" />
+  </div>
+</template>
+
+<style scoped>
+.index {
+}
+</style>
